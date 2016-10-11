febb102c566b6f30c8892e0dc16177cbdea72044
Serdar Dogruyol <dogruyolserdar@gmail.com>
2016-10-11 11:34:47 +0300

    Escape double quotes. Fixes #2.

--- spec/tren_spec.cr
+++ spec/tren_spec.cr
@@ -12,7 +12,6 @@ describe Tren do
   it "should overload method" do
     get_users("fatih", 2).should eq("select * from users where name = 'fatih' and age = 2")
   end
-
   # parse multiple queries from a file
   it "should parse first of multiple queries from a file" do
     get_user_info("alper", "t").should eq("select * from users where name = 'alper' and name = 't'")
@@ -22,4 +21,7 @@ describe Tren do
     get_users_info("alpert", 42, 52).should eq("select * from users where name = 'alpert' and age BETWEEN 42 and 52")
   end
 
+  it "should handle double quotes" do
+    get_user_info("Serdar \"sdogruyol\"", "Doğruyol").should eq("select * from users where name = 'Serdar \"sdogruyol\"' and name = 'Doğruyol'")
+  end
 end
